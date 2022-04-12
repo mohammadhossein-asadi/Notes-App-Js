10683f85b5e6cb633df61265279eb94948ee77d7
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-12 14:23:51 +0430

    change

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const addBox = document.querySelector(".add-box"),
   popupBox = document.querySelector(".popup-box"),
   closeIcon = popupBox.querySelector("header i"),
+  titleTag = popupBox.querySelector("input"),
+  descTag = popupBox.querySelector("textarea"),
   addBtn = popupBox.querySelector("button");
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 addBox.addEventListener("click", () => {
   popupBox.classList.add("show");
@@ -13,5 +30,22 @@ closeIcon.addEventListener("click", () => {
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Button click");
+  let noteTitle = titleTag.value,
+    noteDesc = descTag.value;
+
+  if (noteTitle || noteDesc) {
+    // * getiing month, day, year from the current date
+    let dateObj = new Date(),
+      month = months[dateObj.getMonth()],
+      day = dateObj.getDay(),
+      year = dateObj.getFullYear();
+
+    let noteInfo = {
+      title: noteTitle,
+      description: noteDesc,
+      date: `${month} ${day} ${year}`,
+    };
+
+    console.log(noteInfo);
+  }
 });
