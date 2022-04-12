a8210e2652e08b204c00b2d54bbf3d8df221926e
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-12 13:50:23 +0430

    change file

---
+++ script.js
@@ -0,0 +1,6 @@
+const addBox = document.querySelector(".add-box");
+const popupBox = document.querySelector(".popup-box");
+
+addBox.addEventListener("click", () => {
+  popupBox.classList.add("show");
+});
