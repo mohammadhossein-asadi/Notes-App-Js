c9755e734d521c864d430bff1aad28617efbeab2
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-12 14:02:56 +0430

    commit

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const addBox = document.querySelector(".add-box");
-const popupBox = document.querySelector(".popup-box");
+const addBox = document.querySelector(".add-box"),
+  popupBox = document.querySelector(".popup-box"),
+  closeIcon = popupBox.querySelector("header i"),
+  addBtn = popupBox.querySelector("button");
 
 addBox.addEventListener("click", () => {
   popupBox.classList.add("show");
+});
+
+closeIcon.addEventListener("click", () => {
+  popupBox.classList.remove("show");
+});
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Button click");
 });
